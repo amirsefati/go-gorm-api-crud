56f69bbf2ad3b49c9ae942d1555910a7f6e8052e
Amir <sefati100@gmail.com>
2025-01-05 21:49:49 +0330

    delete and create endpoint

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -2,16 +2,44 @@ package controllers
 
 import (
 	"bookstore/pkg/models"
+	"bookstore/pkg/utils"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 var NewBook models.Book
 
-func GetBook(w http.ResponseWriter, r *http.Request) {
+func getBooks(w http.ResponseWriter, r *http.Request) {
 	NewBook := models.GetAllBooks()
 	res, _ := json.Marshal(NewBook)
 	w.Header().Set("content", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func getBookById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	bookId := params["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	createBook := &models.Book{}
+	utils.ParsBody(r, createBook)
+	b := createBook.CreateBook()
+	res, _ := json.Marshal(b)
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
